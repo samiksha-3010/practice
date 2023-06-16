27a9ce4161feaf72b74d4b7717e6c42121d533fb
samiksha-3010 <samiksha30102@gmail.com>
2023-06-16 21:17:00 +0530

    updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // var  num1= 34;
 // var num2 = 34; 
 
-// if (num1  == num2){
+// if (num1  === num2){
 //     console.log("equl")
   
 // }
@@ -43,14 +43,86 @@
 //     console.log("insidefalse")
 //  }
 
-function checkValue(a,b){
-    if(a===  1)
-    if(b=== 2)
-    console.log("a is 1 and b is 2");
- else
- console.log("a is not 1");
+// function checkValue(a,b){
+//     if(a===  1)
+//     if(b=== 2)
+//     console.log("a is 1 and b is 2");
+//  else
+//  console.log("a is not 1");
+// }
+
+// var num1 = 124
+// var num2 = 765
+// if (num1 === num2){
+//         console.log("equl")
+    
+//      }
+//      else{
+//         console.log("notequl")
+//      }
+                //   =======Example========
+
+// var name = 'samiksha yadav';
+// var gender = "feamle";
+// if(gender == "female"){
+//     console.log("Hello Ms. " + name);
+
+// }else{
+//     console.log("Hello Mr. " + name);
+
+// }
+// var name = 'samiksha yadav';
+// var gender = "male";
+
+// if(gender = "male"){
+//     console.log("Hello Mr."+ name);
+// }else{
+//     console.log("Hello Ms."+ name);
+
+// }
+
+   
+// varx = 10
+// if(16) {
+//     console.log("x is same ");
+// }
+// else{
+//     console.log("x is not same")
+// }
+   
+
+var per =59;
+if(per >=80 && per<= 100){
+    console.log("you are in merit.");
+
+}
+else if (per >=60 && per<=80){
+    console.log("You are first diviesion.");
+}
+else if (per >=33 && per<=60){
+    console.log("You are second diviesion.");
+}
+else if (per >=33 && per<=45){
+    console.log("You are third diviesion.");
+}
+else if (per <33){
+    console.log("You are fail.");
+}
+else {
+    console.log("Please inter valid percentge");
+
 }
 
 
+if (time<10);{
+    console.log("good morning");
+}
+else if (time<20){
+    console.log("good day");
+}
+else {
+    console.log("good evening");
+
+}
 
 
