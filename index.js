4ee23c421ef04f53e22e37981575951894bbf916
samiksha-3010 <samiksha30102@gmail.com>
2023-06-16 16:51:18 +0530

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
-var  num1= 34;
-var num2 = 344; 
+// var  num1= 34;
+// var num2 = 34; 
 
-if (num1  != num2){
-    console.log("equl")
+// if (num1  == num2){
+//     console.log("equl")
   
-}
-else {
-    console.log("notequl")
+// }
+// else {
+//     console.log("notequl")
 
+// }
+
+// var  num1= 45;
+// var   num2= 41;
+// var   num3=  49;
+
+//  if (num1 === num2){
+//     console.log("equl")
+
+//  }
+//  else{
+//     console.log("notequl")
+//  }
+ 
+//  var nam1=  34;
+//  var num2=  45;
+
+//  if(34>45){
+//     console.log("true")
+    
+//  }
+//  else{
+//     console.log("false")
+//  }
+//  var num1= 234;
+//  var num2= 785;
+
+//  if(9>10 || 46> 45){
+//     console.log("insidetrue")
+
+//  }
+//  else{
+//     console.log("insidefalse")
+//  }
+
+function checkValue(a,b){
+    if(a===  1)
+    if(b=== 2)
+    console.log("a is 1 and b is 2");
+ else
+ console.log("a is not 1");
 }
+
+
+
+
