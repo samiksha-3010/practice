87c7babaa00b22c67cecf94ebaeb638fce0585e2
samiksha-3010 <samiksha30102@gmail.com>
2023-06-20 19:56:40 +0530

    done

--- loop.js
+++ loop.js
@@ -1,11 +1,83 @@
-var array = [3,45,6,7,7];
+// var array = [3,45,6,7,7];
 
 
+// for( var i =0;i<array.length;i++  ){
+//     console.log(array[i])
+
+// }
+// for ( var i=0;i<array.length;i++){
+//     console.log(array[i]);
+// }
 
 
+// var array = [2,3,4,6];
+
+// for (var i=0;i<array.length;i++){
+//     console.log(array[i]);
+// }                             
+                                //  =====  + , - , * , / 
+
+// var number1 = 10;
+// var  number2 = 20;
+
+// var addition = number1 + number2;
+// console.log (addition);
+
+// var sub = number1 - number2;
+// console.log(sub)
+
+// var mul = number1 * number2;
+// console.log(mul)
+
+// var div = number1 / number2
+// console.log(div);
+
+// var i = 1;
+//  console.log(i); i = i + 1;
+// i++;
+// i++;
+// i++;
+// console.log(i);
 
 
-for( var i =3;i<array.length;i++  ){
-    console.log(array[i])
+// var i = 1;
+// console.log(i)
+// i = i + 1;
+// i++;
+// i++;
+// i++;
+// i++;
+// console.log(i)
+                                //   <==>
 
-}
+// var number1 = 100;
+// var number2  = 120;
+// var number3 = 200;
+// console.log(number1 < 150)
+
+
+// console.log(number2 >140);
+
+// console.log(number3>120);
+
+                            // opreator   &&  
+
+  var  num1 = 123;
+  var num2 = 123;
+    console.log (num1==num2);
+    console.log (num1 ===num2);                       
+      
+
+    var  num1 = 98;
+    console.log(num1 > 50 && num1 < 99 && num1 < 10);
+    console.log(num1 > 50  || num1 < 90 || num1 < 20 );
+                           
+                            
+                            
+                          
+                            
+
+                           
+                                                     
+
+
