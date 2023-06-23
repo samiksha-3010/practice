5f65bcd417dd4e103fd972c373f5cc13ea5c0351
samiksha-3010 <samiksha30102@gmail.com>
2023-06-23 13:40:20 +0530

    updates

---
+++ opreaters.js
@@ -0,0 +1,13 @@
+let a = 45;
+let b = 7;
+
+// console.log("a+b",-a+b)
+// console.log("a-b",-a-b)
+// console.log("a/b",-a/b)
+// console.log("++b", - ++b);
+// console.log("a++",- ++a)
+
+
+let x=3;
+let y=5;
+console.log(x<y || x ==5);
