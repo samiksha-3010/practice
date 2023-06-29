a5c0bb24825ddbac765b18056352b049461e2c5c
samiksha-3010 <samiksha30102@gmail.com>
2023-06-29 13:14:37 +0530

    done

--- arraypratice.js
+++ arraypratice.js
@@ -74,13 +74,13 @@
 // console.log(reducingArr);
 
 
-let redVal =[2, 4, 7, 8, 9, 12, 34];
-let reducingArr = redVal. reduce((acc, curr)=>{
-    return acc + curr
-    return acc;
-    return curr;
-} ,10)
-console.log (reducingArr);
+// let redVal =[2, 4, 7, 8, 9, 12, 34];
+// let reducingArr = redVal. reduce((acc, curr)=>{
+//     return acc + curr
+//     return acc;
+//     return curr;
+// } ,10)
+// console.log (reducingArr);
 
 
 // ****************MAP FILTER REDUCE CHAINING
@@ -98,3 +98,120 @@ console.log (reducingArr);
 //     return acc + curr;
 //   });
 // console.log(newArr);
+
+// let allInOne [2, 3,6,8,9 ];
+// let newArr = allInOne 
+// .map((e) =>{
+//     return e+5 ;
+
+// } );
+// .filter((e) => {
+//     return acc + Curr;
+// });
+// console.log(newArr);
+
+//==============================FOR EACH
+// is not chainable
+
+// let arr = [5, 10, 55, 22, 3];
+
+// arr.forEach((element, index, array) => {
+//   //   console.log(element);
+//   //   console.log(index);
+//   //   console.log(array);
+//   //   return element;
+// });
+// console.log(newArr);
+
+// let arr = [4, 27, 55, 22, 3];
+// arr. forEach((element, index, array)=>{
+//     console.log (element);
+//     console.log (index);
+//     console.log(array)
+//     return element;
+// })
+// console.log(newArr);
+
+
+//=====================SORT METHOD
+
+// let arr = [ 5, 1, 10, 11, 7];
+// arr .sort();
+// console.log(arr);
+
+// arr.sort((a,b)=>{
+//     return b-a;     //descending
+// });
+// arr.sort ((a,b)=>{
+//     return a-b;  //acesending
+// })
+// console.log(arr);
+
+// =================CONCAT METHOD
+
+// let arr1 = [2, 5, 6, 8];
+// let arr2 = [10, 5, 3, 8];
+// // #1
+// // let arr3 = arr1.concat(arr2);
+// // #2
+// // let arr3 = [arr1, arr2];
+// // #3
+// // let arr3 = [...arr1, ...arr2];
+// // console.log(arr3);
+
+// let newArr = arr1 + "," + arr2;
+// console.log(newArr);
+
+
+
+
+// let arr1 = [2,4,6,8];
+// let arr2 = [10,5,3,8];
+// let arr3 = arr1.concat(arr2);
+// let arr3 = [arr1, arr2];
+
+// let newArr = [...arr1, ...arr2];
+// console.log(newArr);
+
+// let newArr= arr1+ ","+arr2;
+// console.log(newArr);
+
+
+// ***********************EVERY METHOD
+
+// let arr = [2, 5, 6, 8, 7, 9];
+
+// let everyArr = arr.every((element) => {
+//     // return element >= 2;
+//     // return element < 2;
+// });
+
+// console.log(everyArr);
+
+// let arr = [2, 4, 7, 6, 9,];
+// let everyArr arr.every((element) => {
+//     // return element >=2;
+//     // return element <2;
+// });
+// console.log(everyArr);
+
+//======================SOME METHOD
+
+// let arr = [2, 4, 6, 8, 9, ];
+// let someArr = arr.some((element) =>{
+//  return element < 1 ;
+// })
+// console.log(someArr);
+
+// ===========================INDEXOF
+
+// let arr = [5, 8, 6, 7, 9, 10, 20, 30, 54, 12, 5, 6, 865, 4];
+
+// console.log(arr.indexOf(30));
+// console.log(arr.length);
+// console.log(arr.indexOf(arr.length));
+
+// let arr = [ 5,  6,  7, 8, 4, 3, 2, 23, 56, 89 ];
+// console.log(arr.indexOf)((30));
+// console.log(arr.length);
+// console.log(arr.indexOf(arr.length));
