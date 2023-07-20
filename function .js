c39fbba1a1c9972732113b4e9b24c7ae6d52a9d1
samiksha-3010 <samiksha30102@gmail.com>
2023-07-21 00:52:10 +0530

    updates

--- function .js
+++ function .js
@@ -1,6 +1,65 @@
 
-var a=5;
-var b=3;
-var c=3;
-console.log(a)
+// var a=5;
+// var b=3;
+// var c=3;
+// console.log(a)
 
+//    var target = 54
+// var array = [9, 3, 4, 5, 6, 7, 8, 9,1]
+// for(var i=0; i<array.length; i++){
+//     // console.log(array[i])
+//     for(var j=i; +1< array.length; j++){  
+//     if(array[i] + array[j]  == target){
+//         console.log(array[i],array[j],"got!" )
+
+
+//     }
+//     console.log(array[i],"+", array[j],"=", array[i]+array[j]);
+// }
+// }
+
+// Q Given an target find two number from array which subtration is target . 
+
+// var target = 93;
+// var array = [3, 54, 65, 32, 6, 23, 56, 76, 17, 100, 7];
+// // 100,7
+
+// for(var i = 0; i<array.length; i++){
+//     console.log([i]);
+//     for(var j=i; i<array.length; j++){
+//         if(array[i]- array[j]==target ){
+//             console.log(array[i],array[j], "got!")
+
+//         }
+//         console.log(array[i],"-",array[j],"=", array[i]+array[j])
+
+//     }
+// }
+
+// var target = 93;
+// var array = [3, 54, 65, 32, 6, 23, 56, 76, 17];
+// // result "3 17"
+// for (var i = 0; i < array.length; i++) {
+//     // console.log(array[i])
+//     for (var j = i + 1; j < array.length; j++) {
+//         if (array[i] + array[j] == target) {
+//             console.log(array[i], array[j], "got!")
+//         }
+//         console.log(array[i], " + ", array[j], " = " ,array[i] + array[j])
+//     }
+// }
+
+// Loops - for
+// Q. Print numbers from 1- 10;
+// var i =8;
+// i++;
+// console.log(i)
+
+// var i = 6;
+// i++;
+// console.log(i)
+
+
+for (var i = 1; i <= 10; i++) {
+    console.log("Hello", i);
+}
