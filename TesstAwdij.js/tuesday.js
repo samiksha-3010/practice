0198f9f744122fdd07e738dd1cfa816c9184ff78
samiksha-3010 <samiksha30102@gmail.com>
2023-09-28 18:37:01 +0530

    done

---
+++ TesstAwdij.js/tuesday.js
@@ -0,0 +1,9 @@
+// Events 
+
+// There are many event in Js like click, mouseenter, mouseover, mouseleave, etc
+
+// This event perform once the website or page loads for the first time
+
+
+
+
--- acynsawait.js
+++ acynsawait.js
@@ -14,20 +14,20 @@
 //     },3000)
 // }
 
-async function prom() {
-    let one = new Promise((resolve) => {
-      setTimeout(() => {
-        resolve("promise 1"); // execute 2nd
-      }, 6000);
-    });
+// async function prom() {
+//     let one = new Promise((resolve) => {
+//       setTimeout(() => {
+//         resolve("promise 1"); // execute 2nd
+//       }, 6000);
+//     });
 
-     function one(){
-        setTimeout(() =>{
-            console.log("one");
-         } ,3000);
-    };
+//      function one(){
+//         setTimeout(() =>{
+//             console.log("one");
+//          } ,3000);
+//     };
 
-}
+// }
 
 
     // find odd number
@@ -224,7 +224,7 @@ async function prom() {
 //   }
 // }
 
-var array  = [23, 56, 34, 23, 56, 89, 10];
+// var array  = [23, 56, 34, 23, 56, 89, 10];
 // var trget = 24;
 
 // function sumThree (array, target ){
@@ -245,3 +245,61 @@ var array  = [23, 56, 34, 23, 56, 89, 10];
 // }
 
 
+
+// for  ( i=5;i <= 50; i+=5) {
+//     console.log(i)
+// } 
+
+
+// var arr = ["ab", "bc", "cd", "de", "ef", "fg", 1, 2, 1, 2, 1, 2, 1, 1, 1];
+
+
+// for (ar of arr) {
+//   console.log(ar);
+// }
+
+
+// let target = 50;
+
+
+// let array = [10, 20, 30, 15, 45, 2, 9, 5]; // by adding 2 number
+
+
+// for (i = 0; i< array.length; i++){
+//     console.log(array[i])
+
+
+//     for(j = 0; j< array.length; i++){
+//         console.log(array[j])
+
+//         if (array[i] +array[j] === value){
+//             console.log(array[i], "-",array[j],"=",value);
+//         }
+// }
+
+// }
+
+
+// let total = [12,34,56,79,12]
+
+// let sum = 0;
+
+// for(let i=0; i< total.length; i++){
+//     sum += total[i]
+// }
+// console.log(sum)
+
+// let arr = [12,34,56,78,12 ]
+// let newArray  = arr.filter((num,i)=>{
+// return num>58
+// }) 
+
+// console.log(newArray)
+let arr = [12,34,56,78,12 ]
+let mapping = arr.map((val)=>{
+    if(val <=50){
+        return val
+    }
+})
+
+console.log(mapping)
