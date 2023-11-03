9ddb4bbf5eae9b9ef5533e17e014828a6d03840f
samiksha-3010 <samiksha30102@gmail.com>
2023-11-03 12:41:03 +0530

    done

---
+++ secondBatch/day-4.js
@@ -0,0 +1,111 @@
+// conditional statement
+
+// if else
+// Switch
+
+// if else
+
+// syntax()
+
+
+// if(conditions){
+//     statement 1
+// }else{
+//     statement 2
+// }
+
+// Switch
+// switch ("value2") {
+//     case "value1":
+//         console.log("hji")
+//         break;
+
+//     case "value2":
+//         console.log("hji")
+//         break;
+
+//     case "value3":
+//         console.log("hji")
+//         break;
+
+//     default:
+//         console.log("hji")
+//         break;
+// }
+
+
+// Q 1 Find user age is greater than 19 or not
+
+// var userAge = 20 
+// console.log(userAge > 22)  // true 
+// if(userAge > 22){
+//     console.log("U are allowd for Driving liceance")
+// }else{
+//     console.log("U are not allowed a driving liceance")
+// }
+
+//     // Q 2 Find user age is greater than 19 and lesser than 90 or not 
+
+//     var userAge = 23;
+//     if(userAge > 11 && userAge < 90){
+//         console.log("your age is grater than 11 and lesser than 90. you are not allow")
+//     }else{
+//         console.log("your is might be 11 or more than 90")
+//     }
+
+//     // Q 3 Find user age is greater than 19 and lesser than 90 then allow for a Dl
+//     //  if age is lesser than 19 then  return "u are under age"
+//     //  if age is more than 90 then return "u are over age"
+//     //  if age is 19 then return "you are allow Dl"
+
+
+// var userAge =19;
+// if(userAge > 19 && userAge < 90){
+//     console.log("u are allowed")
+// }else if (userAge < 19 ){
+//     console.log("u are a under age")
+// }else if (userAge >= 90){
+//     console.log("u are over age")
+// }else {
+//     console.log("u are allow a DL")
+// }
+
+// // Datatypes String , Number,Array,boolean,
+// // undefined
+// // Array
+
+// var  myName 
+// console.log(myName)
+// console.log(typeof myName) // unddefined
+
+// var myName = "neha"
+// console.log(myName)
+// console.log (typeof myName) // define
+
+
+// var myData = ["samiksha","22",true,"hello",23421,[1,2,3,4,5]]
+// console.log(myData)
+// console.log(myData.length)
+// console.log(myData[0])
+// console.log(myData[1])
+// console.log(myData[2])
+// console.log(myData[3])
+// console.log(myData[0][6])
+
+// // push and pop method
+
+// var fruits = ['banana', "apple",'mango']
+// console.log(fruits)
+
+// fruits.push("papaya")
+// console.log("fruits")  // add the element at the end of array
+
+var number = [23, 23, 45, 67,89]
+number.pop() // remove last element
+number.pop()
+number.pop() // remove last element
+number.pop()
+number.pop() // remove last element
+console.log(number)
+
+
---
+++ secondBatch/day-6.js
@@ -0,0 +1,45 @@
+// Revares for loop
+//20-1
+for (var i=20; i>0;i--){
+    console.log(i)
+}
+
+// 200-20 ,sequence 5
+
+for (var i = 200;i >=20;i -=5){
+    console.log(i)
+}
+
+// 100-20 sequence 2
+
+for (var i = 20; i>0;i-=5){
+    console.log(i)
+}
+
+var student = ["samiksha","pratiksha","kajal","vibha","aarush"]
+ console.log(student[4])
+
+ for (var i = 0;i < student.length; i++){
+    console.log([i],i)
+ }
+
+ var number = [2,3,4,5,6,7,8]
+ for(var i = number.length -1; i>= 0; i--) {
+    console.log(number[i],i)
+ }
+
+ var number = [2,1,4,3,5,7,8,9]
+ for(var i= number.length -2; i>=0; i--){
+    console.log(number[i])
+ }
+
+var number = [ 10,20,30,40,50,60]
+for (var i = number.length -2;i>= 0; i-=10){
+    console.log(number[i])
+}
+
+var number = [1,2,3,4,5,6]
+
+for (var i=10;i > 1; i-=5){
+    console.log(number[i])
+}
